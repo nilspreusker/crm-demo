1115d40f0eb8f89e41c0547debd60ae0458a4fa5
Nils Preusker <nils.preusker@movingimage24.de>
2013-06-18 18:43:51 +0200

    adding spring security on server side and basic sign-in mechanism to AngularJS application
    sign-in mechanism still lacks user validation (needs to be added both
    on client and on server side)

--- src/main/java/com/mycompany/boundary/CrmDemoApplication.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.boundary;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class CrmDemoApplication extends Application {
-
-}
